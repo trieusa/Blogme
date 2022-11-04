527a63a19fdf8013ad48cac3db3bae1b4a868362
trieusa <117133103+trieusa@users.noreply.github.com>
2022-11-04 15:29:33 +0700

    Add files via upload

---
+++ Migrations/20221104050621_Migrations.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBlog.Migrations
+{
+    public partial class Migrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblTag",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblTag", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUser",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUser", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblCategory",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedById = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCategory", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_tblCategory_tblUser_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "tblUser",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblArticle",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ViewCount = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblArticle", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_tblArticle_tblCategory_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "tblCategory",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_tblArticle_tblUser_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "tblUser",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblArticleTag",
+                columns: table => new
+                {
+                    ArticleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblArticleTag", x => new { x.ArticleId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_tblArticleTag_tblArticle_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "tblArticle",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tblArticleTag_tblTag_TagId",
+                        column: x => x.TagId,
+                        principalTable: "tblTag",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblComment",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ArticleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblComment", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_tblComment_tblArticle_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "tblArticle",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tblComment_tblUser_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "tblUser",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblrticleLiker",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ArticleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LikeAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblrticleLiker", x => new { x.ArticleId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_tblrticleLiker_tblArticle_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "tblArticle",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tblrticleLiker_tblUser_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUser",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblArticle_AuthorId",
+                table: "tblArticle",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblArticle_CategoryId",
+                table: "tblArticle",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblArticleTag_TagId",
+                table: "tblArticleTag",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblCategory_CreatedById",
+                table: "tblCategory",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblComment_ArticleId",
+                table: "tblComment",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblComment_AuthorId",
+                table: "tblComment",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblrticleLiker_UserId",
+                table: "tblrticleLiker",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblArticleTag");
+
+            migrationBuilder.DropTable(
+                name: "tblComment");
+
+            migrationBuilder.DropTable(
+                name: "tblrticleLiker");
+
+            migrationBuilder.DropTable(
+                name: "tblTag");
+
+            migrationBuilder.DropTable(
+                name: "tblArticle");
+
+            migrationBuilder.DropTable(
+                name: "tblCategory");
+
+            migrationBuilder.DropTable(
+                name: "tblUser");
+        }
+    }
+}
