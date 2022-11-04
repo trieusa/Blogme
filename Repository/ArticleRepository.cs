4ecbf158c1eed9c2a8cfd6bbacd5ca26dd1c88d2
trieusa <117133103+trieusa@users.noreply.github.com>
2022-11-04 15:13:56 +0700

    Add files via upload

---
+++ Repository/ArticleRepository.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyBlog.Models;
+using MyBlog.Data;
+using MyBlog.Dto.Article;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Repository
+{
+    public class ArticleRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public ArticleRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public ArticleDto InsertArticle(Article article)
+        {
+            _appDbContext.Articles.Add(article);
+            _appDbContext.SaveChanges();
+            var result = new ArticleDto()
+            {
+                ID=article.ID,
+                Title = article.Title,
+                Content = article.Content,
+                ViewCount = article.ViewCount,
+                AuthorId = article.AuthorId,
+                CategoryId =article.CategoryId,
+            };
+            return result;
+        }
+
+        public async Task<List<ArticleDto>> GetListArticle()
+        {
+            return await _appDbContext.Articles.Select(article => new ArticleDto()
+            {
+                ID=article.ID,
+                Title = article.Title,
+                Content = article.Content,
+                ViewCount = article.ViewCount,
+                AuthorId = article.AuthorId,
+                CategoryId =article.CategoryId,
+            }).AsNoTracking().ToListAsync();
+        }
+
+        public async Task<bool> DeleteArticle(Guid Id)
+        {
+            var article = await _appDbContext.Articles.FirstOrDefaultAsync(article => article.ID == Id);
+
+            if (article == null)
+            {
+                return false;
+            };
+
+            _appDbContext.Articles.Remove(article);
+            await _appDbContext.SaveChangesAsync();
+
+            return true;
+
+        }
+
+
+        public async Task<ArticleDto> EditArticle(Guid Id, Article article)
+        {
+
+            var articleExist = await _appDbContext.Articles.FirstOrDefaultAsync(article => article.ID == Id);
+
+            if (articleExist == null)
+            {
+                return null;
+            };
+                articleExist.Title = article.Title;
+                articleExist.Content = article.Content;
+                articleExist.ViewCount = article.ViewCount;
+                articleExist.AuthorId = article.AuthorId;
+                articleExist.CategoryId = article.CategoryId;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return new ArticleDto()
+            {
+                ID=articleExist.ID,
+                Title = articleExist.Title,
+                Content = articleExist.Content,
+                ViewCount = articleExist.ViewCount,
+                AuthorId = articleExist.AuthorId,
+                CategoryId = articleExist.CategoryId,
+            };
+        }
+
+    }
+}
